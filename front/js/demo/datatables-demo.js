84aeae05d78920e9306a66ee3118aeeddee2d8be
Eundms <dms011221@gmail.com>
2021-05-19 20:55:47 +0900

    change cdn

--- front/js/demo/datatables-demo.js
+++ front/js/demo/datatables-demo.js
@@ -1,10 +1,58 @@
 // Call the dataTables jQuery plugin
+
 $(document).ready(function() {
-  $('#dataTable').DataTable();
-});
-$(document).ready(function() {
-  $('#dataTable2').DataTable();
-});
+  $('#dataTable2').DataTable( {
+    dom: 'Bfrtip',
+    buttons: [
+        'copy', 'csv', 'excel', 'pdf', 'print'
+    ],
+      initComplete: function () {
+          this.api().columns().every( function () {
+              var column = this;
+              var select = $("<select class='form-select'><option value=''></option></select>")
+                  .appendTo( $(column.footer()).empty() )
+                  .on( 'change', function () {
+                      var val = $.fn.dataTable.util.escapeRegex(
+                          $(this).val()
+                      );
+
+                      column
+                          .search( val ? '^'+val+'$' : '', true, false )
+                          .draw();
+                  } );
+              
+              column.data().unique().sort().each( function ( d, j ) {
+                  select.append( '<option value="'+d+'">'+d+'</option>' )
+              } );
+          } );
+      }
+  } );
+} );
 $(document).ready(function() {
   $('#dataTable3').DataTable();
 });
+
+$(document).ready(function() {
+  $('#dataTable').DataTable( {
+      initComplete: function () {
+          this.api().columns().every( function () {
+              var column = this;
+              var select = $('<select><option value=""></option></select>')
+                  .appendTo( $(column.footer()).empty() )
+                  .on( 'change', function () {
+                      var val = $.fn.dataTable.util.escapeRegex(
+                          $(this).val()
+                      );
+
+                      column
+                          .search( val ? '^'+val+'$' : '', true, false )
+                          .draw();
+                  } );
+
+              column.data().unique().sort().each( function ( d, j ) {
+                  select.append( '<option value="'+d+'">'+d+'</option>' )
+              } );
+          } );
+      }
+  } );
+} );
