07630c0fab7e94a98673fda082be262782fa687d
SeungJooKim <seoungjoo01@gmail.com>
2021-07-25 20:21:32 +0900

    Add credit Page

---
+++ client/src/pages/Credit/CreditShow.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+function CreditShowTable() {
+  return (
+    <div className="table-responsive">
+      <table className="table" style={{ background: 10 }}>
+        <thead>
+          <tr>
+            <th scope="col">신용등급 분포!</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1등급</td>
+            <td>아이1, 아이2, 아이3</td>
+          </tr>
+          <tr>
+            <td>2등급</td>
+            <td>아이1, 아이2, 아이3</td>
+          </tr>
+          <tr>
+            <td>3등급</td>
+            <td>아이1, 아이2, 아이3</td>
+          </tr>
+          <tr>
+            <td>4등급</td>
+            <td>아이1, 아이2, 아이3</td>
+          </tr>
+          <tr>
+            <td>5등급</td>
+            <td>아이1, 아이2, 아이3</td>
+          </tr>
+          <tr>
+            <td>6등급</td>
+            <td>아이1, 아이2, 아이3</td>
+          </tr>
+          <tr>
+            <td>7등급</td>
+            <td>아이1, 아이2, 아이3</td>
+          </tr>
+          <tr>
+            <td>8등급</td>
+            <td>아이1, 아이2, 아이3</td>
+          </tr>
+          <tr>
+            <td>9등급</td>
+            <td>아이1, 아이2, 아이3</td>
+          </tr>
+          <tr>
+            <td>10등급</td>
+            <td>아이1, 아이2, 아이3</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+export default CreditShowTable;
