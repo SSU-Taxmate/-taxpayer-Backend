df7adb611bf3ed5259a30a7b2b81ea2c9c5f1929
noino0819 <noino0819@naver.com>
2021-07-28 11:40:28 +0900

    login Router

---
+++ server/routes/middlewares.js
@@ -0,0 +1,23 @@
+const path = require('path'); // 현재 프로젝트의 경로
+
+exports.isLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated()) { // 로그인 상태 확인
+        next(); // 로그인 상태이면 다음 미들웨어로 넘어감
+    } else { // 로그인 안된 상태
+        // res.status(403).send('로그인 필요');
+        res.render(path.join(__dirname, '../views/login.ejs'), {
+            title: 'TaxPayer 로그인',
+            loginFailed: req.session.loginFailed
+        });
+        req.session.destroy(function(err) {})
+    }
+};
+
+exports.isNotLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        next();
+    } else {
+        const message = encodeURIComponent('로그인한 상태입니다.');
+        res.redirect(`/?error=${message}`);
+    }
+};
