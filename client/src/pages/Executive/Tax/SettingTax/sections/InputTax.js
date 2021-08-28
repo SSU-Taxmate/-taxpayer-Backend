1fc2e81d7a703a897a73b27c75d8e6437b23fd15
SeungJooKim <seoungjoo01@gmail.com>
2021-08-29 00:54:02 +0900

    edit Tax Front & reactive

--- client/src/pages/Executive/Tax/SettingTax/sections/InputTax.js
+++ client/src/pages/Executive/Tax/SettingTax/sections/InputTax.js
@@ -1,28 +1,36 @@
-import React from 'react'
-import TextField from '@material-ui/core/TextField';
-import InputAdornment from '@material-ui/core/InputAdornment';
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import { makeStyles } from "@material-ui/core/styles";
 
 function InputTax({ title, value, name, readOnly, onChange, unit }) {
   //console.log('inputtax', value)                step="5"
-
+  //스타일
+  const useStyles = makeStyles({
+    taxInput: {
+      marginRight: "3rem",
+      marginBottom: "1rem",
+      width: "13rem",
+    },
+  });
+  const classes = useStyles(); //스타일 사용
   return (
-  
-      <TextField
-        id={`tax-${title}`}
-        label={title}
-        type="number"
-        variant="outlined"
-        defaultValue={value}
-        InputProps={{
-          endAdornment: <InputAdornment position="end">{unit}</InputAdornment>,
-          inputProps: { min: 0, max: 100 },
-          readOnly:readOnly
-        }}
-        name={name}
-        onChange={onChange}
-      />
-
-  )
+    <TextField
+      className={classes.taxInput}
+      id={`tax-${title}`}
+      label={title}
+      type="number"
+      variant="outlined"
+      defaultValue={value}
+      InputProps={{
+        endAdornment: <InputAdornment position="end">{unit}</InputAdornment>,
+        inputProps: { min: 0, max: 100 },
+        readOnly: readOnly,
+      }}
+      name={name}
+      onChange={onChange}
+    />
+  );
 }
 
-export default InputTax
+export default InputTax;
