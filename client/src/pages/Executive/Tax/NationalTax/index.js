1e1da8bab5bdc0514db8ab8c89efdbce8dc692ed
SeungJooKim <seoungjoo01@gmail.com>
2021-08-29 18:51:56 +0900

    Edit Executive Front

--- client/src/pages/Executive/Tax/NationalTax/index.js
+++ client/src/pages/Executive/Tax/NationalTax/index.js
@@ -1,36 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 //Navigation
-import Topbar from '../../../../components/Navigation/Topbar';
-import Footer from '../../../../components/Footer'
-import PageHeading from '../../../../components/PageHeading';
-import BudgetSection from './BudgetAll/BudgetSection'
-import BudgetStatistics from './BudgetAll/BudgetStatistics'
-import ScrollToTop from '../../../../components/Scroll';
-import ChartArea from './BudgetCategory/ChartArea';
-import TaxTypeArea from './BudgetCategory'
+import Topbar from "../../../../components/Navigation/Topbar";
+import Footer from "../../../../components/Footer";
+import PageHeading from "../../../../components/PageHeading";
+import BudgetSection from "./BudgetAll/BudgetSection";
+import BudgetStatistics from "./BudgetAll/BudgetStatistics";
+import ScrollToTop from "../../../../components/Scroll";
+import ChartArea from "./BudgetCategory/ChartArea";
+import TaxTypeArea from "./BudgetCategory";
 class NationalTax extends Component {
-
   render() {
     return (
       <>
         <div id="wrapper">
-
           <div id="content-wrapper" className="d-flex flex-column">
             <div id="content">
               <Topbar />
               <div className="container-fluid">
                 <PageHeading title="나라 세금 통계" />
                 {/*<!--재정 상황-->*/}
-                <div className='account-card shadow bg-white' style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+                <div
+                  className="account-card shadow bg-white"
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-evenly",
+                    flexWrap: "wrap",
+                  }}
+                >
                   <BudgetSection />
                   <BudgetStatistics />
                 </div>
                 {/*<!--세입/세출-->*/}
-                <div className="account-card shadow justify-content-center bg-white mt-4">
-                <TaxTypeArea/>
-                
-                  <div className='row'>
+                <div className="account-card shadow bg-white mt-4">
+                  <TaxTypeArea />
+                  <hr></hr>
+                  <div className="row mt-4">
                     <ChartArea />
                   </div>
                 </div>
@@ -41,7 +46,7 @@ class NationalTax extends Component {
         </div>
         <ScrollToTop />
       </>
-    )
+    );
   }
 }
 
