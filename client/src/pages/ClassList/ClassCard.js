98392da73f5b103c29376bc5fea6180d5d932ac3
SeungJooKim <seoungjoo01@gmail.com>
2021-09-04 12:15:08 +0900

    Edit ClassList EntryCodeModal

--- client/src/pages/ClassList/ClassCard.js
+++ client/src/pages/ClassList/ClassCard.js
@@ -1,42 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 import { selectClass, selectUser } from "../../redux/_actions";
 import ClassCodeModal from "../../components/Modal/ClassCodeModal";
-import moment from 'moment-timezone';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import DeleteIcon from '@material-ui/icons/Delete';
-
+import moment from "moment-timezone";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: 300,
-
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%", // 16:9
   },
   expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
       duration: theme.transitions.duration.shortest,
     }),
   },
   expandOpen: {
-    transform: 'rotate(180deg)',
+    transform: "rotate(180deg)",
   },
-
 }));
-
 
 function ClassCard(props) {
   const dispatch = useDispatch();
@@ -45,9 +41,8 @@ function ClassCard(props) {
   const classes = useStyles();
 
   return (
-
     <div className="m-4">
-      <Card className={classes.root} >
+      <Card className={classes.root}>
         <Link
           to={`/classes/${props.id}`}
           color="inherit"
@@ -62,8 +57,9 @@ function ClassCard(props) {
         >
           <CardHeader
             title={props.title}
-            subheader={moment(props.date).tz('Asia/Seoul').format('YYYY-MM-DD')}
-          /></Link>
+            subheader={moment(props.date).tz("Asia/Seoul").format("YYYY-MM-DD")}
+          />
+        </Link>
         <CardMedia
           className={classes.media}
           image={props.img}
@@ -76,17 +72,18 @@ function ClassCard(props) {
         </CardContent>
         <CardActions disableSpacing>
           <IconButton aria-label="share">
-            <ClassCodeModal id={`${props.title}displaycode`} entrycode={props.entrycode}></ClassCodeModal>
+            <ClassCodeModal
+              id={`${props.title}displaycode`}
+              entrycode={props.entrycode}
+              className={props.title}
+            ></ClassCodeModal>
           </IconButton>
-          <IconButton aria-label="delete" style={{ marginLeft: 'auto' }}>
+          <IconButton aria-label="delete" style={{ marginLeft: "auto" }}>
             <DeleteIcon />
           </IconButton>
         </CardActions>
-
       </Card>
-
     </div>
-
   );
 }
 
