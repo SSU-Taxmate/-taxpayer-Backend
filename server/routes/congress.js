cc944a2759e5400a320e675ab3fe17c8602acdb1
noino0819 <noino0819@naver.com>
2021-09-08 01:08:09 +0900

    suggest_law get

--- server/routes/congress.js
+++ server/routes/congress.js
@@ -3,6 +3,9 @@
 */
 const express = require('express');
 const { LawSuggest } = require('../models/Law_suggest');
+const { JoinedUser } = require('../models/JoinedUser')
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Types.ObjectId;
 const router = express.Router();
 
 /*
@@ -10,47 +13,92 @@ const router = express.Router();
   : Suggest_law
 */
 router.post('/', (req, res) => {
-    const laws = new LawSuggest(req.body);
-    laws.save((err, doc) => {
-        if (err) return res.json({ success: false, err })
-        return res.status(200).json({ success: true })
+        const laws = new LawSuggest(req.body);
+        laws.save((err, doc) => {
+            if (err) return res.json({ success: false, err })
+            return res.status(200).json({ success: true })
+        })
     })
-})
+    /*
+      [정상] Class별 SuggestLaw 모두 보여주기
+      : Suggest_Law
+        - req.query {classId:}
+    */
 
-/*
-  [정상] Class별 SuggestLaw 모두 보여주기
-  : Suggest_Law
-    - req.query {classId:}
-*/
+router.get('/', async(req, res) => {
+    const classId = req.query.classId
+    try {
+        const studentnum = await JoinedUser.countDocuments({ 'classId': classId }).exec()
 
-router.get('/', (req, res) => {
-    LawSuggest.find(req.query, (err, classlaw) => {
-        const result = classlaw
-            //console.log(result)
-        if (err) return res.status(500).json({ error: err });
-        res.json(result)
-    })
+        const lawsuggest = await LawSuggest.aggregate([{
+                $match: {
+                    "classId": ObjectId(classId)
+                }
+            },
+            {
+                $lookup: {
+                    from: 'users',
+                    let: { initiator: '$initiator' },
+                    pipeline: [{
+                            $match: {
+                                $expr: {
+                                    $eq: ['$$initiator', '$_id']
+                                }
+                            }
+                        },
+                        {
+                            $project: {
+                                'name': 1
+                            }
+                        }
+                    ],
+                    as: 'initiator',
+                }
+            },
+            {
+                $unwind: '$initiator'
+            },
+            {
+                $addFields: {
+                    'numvoter': { $size: '$vote' },
+                    'numpros': {
+                        $size: {
+                            $filter: {
+                                input: '$vote',
+                                as: 'voter',
+                                cond: {
+                                    $eq: ['$$voter.value', true]
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        ])
+        const result = { lawsuggest, studentnum: studentnum }
+        res.send(result);
+    } catch (error) {
+        return res.json({ success: false, err });
+    }
+
 })
 
 /*
   [정상] Suggest_Law수정
   : Suggest_Law
 */
-
 router.put('/', (req, res) => {
-    LawSuggest.updateOne({ _id: req.body._id }, { $set: req.body }, (err, doc) => {
-        if (err) return res.json({ success: false, err });
-        return res.status(200).json({
-            success: true
+        LawSuggest.updateOne({ _id: req.body._id }, { $set: req.body }, (err, doc) => {
+            if (err) return res.json({ success: false, err });
+            return res.status(200).json({
+                success: true
+            })
         })
     })
-
-})
-
-/*
-  [정상] Suggest_Law 삭제 : deleteOne
-*/
-
+    /*
+      [정상] Suggest_Law 삭제 : deleteOne
+    */
 router.delete('/:id', (req, res) => {
     console.log(req.params.id)
     const lawId = req.params.id
@@ -61,6 +109,4 @@ router.delete('/:id', (req, res) => {
         })
     })
 })
-
-
 module.exports = router;
